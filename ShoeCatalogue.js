64c3e23c28b723fe926fd5669e9075c8c21e1d79
Mzizio <mtshalimxolisii@gmail.com>
2017-04-10 16:53:29 +0200

    add files

--- ShoeCatalogue.js
+++ ShoeCatalogue.js
@@ -1,30 +1,51 @@
 var tamplate =  document.getElementById('myTamplate');
 var myDisplay = document.getElementById('display');
 var whichone = document.getElementById('othershoes');
-var Thisone =  document.getElementById('Izihlangu');
+var thisone =  document.getElementById('Izihlangu');
 var othersWear= document.getElementById('wear');
-var LadyWear= document.getElementById('ladyshoe');
+var ladyWear= document.getElementById('ladyshoe');
+var took= document.getElementById('clickSearch');
+
 
 
 
 var myTampleInfor = Handlebars.compile(tamplate.innerHTML);
 var shoes = [
-    {formal : 'formalShoe', price : 900, color : 'Dark', in_stock : 5, image : 'formal.jpg'},
-    {Snaekers  : 'runningShoe', price : 1500, color : 'Brown', in_stock : 7, image : 'nike.jpg'},
+    {formal : 'formalShoe', price : 900, color : 'Brown', in_stock : 5, image : 'formal.jpg'},
+    {Snaekers  : 'runningShoe', price : 1500, color : 'white', in_stock : 7, image : 'nike.jpg'},
     {Stars  : 'allstarTakkie', price : 1400, color: 'Red', in_stock : 4, image : "allstar.jpg"},
     {themazon  : 'workShoe', price : 500, color: 'maroon', in_stock : 3, image : 'amazon.jpg'},
   ];
 var display = myTampleInfor({shoes});
-myDisplay.innerHTML = display;
+//myDisplay.innerHTML = display;
 
-var filteredList = [];
-for (var i=0;i<shoes.length;i++){
-  var shoes = shoes[i];
-  if (e.target.value === "ALL"){
-    filteredList.push(Shoes);
+var outPut = function searchShoes(){
+  var filteredList = [];
+  for (var i=0;i<shoes.length;i++){
+
+    if ( thisone.value === shoes[i].color){
+      filteredList.push(shoes);
+    }
   }
-}
 
-//Now use this with my template:
-var filteredShoesHTML = template({ shoes : filteredList});
-display.innerHTML = filteredShoesHTML;
+  //Now use this with my template:
+  var filteredShoesHTML = myTampleInfor({ shoes : filteredList});
+//  myDisplay.innerHTML = filteredShoesHTML;
+  console.log("me");
+
+};
+
+took.addEventListener("click", function(){
+  var filteredList = [];
+  for (var i=0;i<shoes.length;i++){
+
+    if ( thisone.value === shoes[i].color){
+      filteredList.push(shoes[i]);
+    }
+  }
+
+  //Now use this with my template:
+  var filteredShoesHTML = myTampleInfor({ shoes : filteredList});
+  myDisplay.innerHTML = filteredShoesHTML;
+  console.log("me");
+});
